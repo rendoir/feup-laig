9aa782917137872a48351d1638ca7cf34ef5f54d
rendoir <up201503822@fe.up.pt>
2017-10-05 21:04:37 +0100

    Fixed sphere issues by using CGRA's code

--- primitives/semisphere.js
+++ primitives/semisphere.js
@@ -15,34 +15,34 @@ SemiSphere.prototype = Object.create(CGFobject.prototype);
 SemiSphere.prototype.constructor = SemiSphere;
 
 SemiSphere.prototype.initBuffers = function() {
-    var angle = (2 * Math.PI) / this.slices;
-
     this.vertices = [];
     this.indices = [];
     this.normals = [];
     this.texCoords = [];
-    var diameter = 2 * this.radius;
-    for (stack = 0; stack <= this.stacks; stack++) {
-        h = this.radius * Math.sqrt(stack / this.stacks);
-        sub_radius = Math.sqrt((this.radius * this.radius) - (h * h));
-        for (i = 0; i <= this.slices; i++) {
-            var x = Math.cos(i * angle) * sub_radius;
-            var y = Math.sin(i * angle) * sub_radius;
 
-            this.vertices.push(x, y, h);
-            this.normals.push(x, y, h);
-            this.texCoords.push((x + this.radius) / (diameter), (-y + this.radius) / (diameter));
+    var ang = Math.PI * 2 / this.slices;
+    var angEsf = Math.PI / 2 / this.stacks;
 
-        }
-    }
-    for (stack = 0; stack < this.stacks; stack++) {
-        stack_const = this.slices + 1;
+    for (j = 0; j < this.stacks + 1; j++) {
         for (i = 0; i < this.slices; i++) {
-            this.indices.push(i + 1 + this.slices + (stack * stack_const), (i + 1) + (stack * stack_const), i + (stack * stack_const));
-            this.indices.push(i + this.slices + 1 + (stack * stack_const), (i + this.slices + 2) + (stack * stack_const), i + 1 + (stack * stack_const));
+
+            this.vertices.push(Math.cos(i * ang) * Math.cos(angEsf * j), Math.sin(i * ang) * Math.cos(angEsf * j), Math.sin(angEsf * j));
+            this.normals.push(Math.cos(i * ang) * Math.cos(angEsf * j), Math.sin(i * ang) * Math.cos(angEsf * j), Math.sin(angEsf * j));
+            this.texCoords.push((Math.cos(i * ang) * Math.cos(angEsf * j) + 1)/2, (Math.sin(i * ang) * Math.cos(angEsf * j) + 1)/2 );
         }
     }
 
+    for (j = 0; j < this.stacks; j++) {
+        for (i = 0; i < this.slices; i++) {
+            if (i == this.slices - 1) {
+                this.indices.push(0 + this.slices * j, 0 + this.slices * (j + 1), 0 + i + this.slices * (j + 1));
+                this.indices.push(0 + this.slices * j, 0 + i + this.slices * (j + 1), 0 + i + this.slices * j);
+            } else {
+                this.indices.push(0 + i + this.slices * j, 1 + i + this.slices * j, 0 + i + this.slices * (j + 1));
+                this.indices.push(1 + i + this.slices * j, 1 + i + this.slices * (j + 1), 0 + i + this.slices * (j + 1));
+            }
+        }
+    }
 
     this.primitiveType = this.scene.gl.TRIANGLES;
     this.initGLBuffers();
--- primitives/sphere.js
+++ primitives/sphere.js
@@ -4,9 +4,9 @@
  */
 function Sphere(scene, radius, slices, stacks) {
     CGFobject.call(this, scene);
-    this.radius = radius || 1;
-    this.slices = slices || 16;
-    this.stacks = stacks || 16;
+    this.radius = radius;
+    this.slices = slices;
+    this.stacks = stacks;
     this.semisphere = new SemiSphere(scene, slices, stacks, radius);
 };
 
@@ -15,6 +15,7 @@ Sphere.prototype.constructor = Sphere;
 
 Sphere.prototype.display = function() {
     this.scene.pushMatrix();
+    this.scene.scale(this.radius, this.radius, this.radius);
     this.semisphere.display();
     this.scene.rotate(Math.PI, 0, 1, 0);
     this.semisphere.display();
