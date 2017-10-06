93cbdbcc96e4e8bce879f6800989eaedd56dd59c
rendoir <up201503822@fe.up.pt>
2017-10-06 15:35:50 +0100

    Fixed spheres. Added sun texture. Other minor fixes to xml

--- primitives/sphere.js
+++ primitives/sphere.js
@@ -7,17 +7,48 @@ function Sphere(scene, radius, slices, stacks) {
     this.radius = radius;
     this.slices = slices;
     this.stacks = stacks;
-    this.semisphere = new SemiSphere(scene, slices, stacks, radius);
+    this.initBuffers();
 };
 
 Sphere.prototype = Object.create(CGFobject.prototype);
 Sphere.prototype.constructor = Sphere;
 
-Sphere.prototype.display = function() {
-    this.scene.pushMatrix();
-    this.scene.scale(this.radius, this.radius, this.radius);
-    this.semisphere.display();
-    this.scene.rotate(Math.PI, 0, 1, 0);
-    this.semisphere.display();
-    this.scene.popMatrix();
+Sphere.prototype.initBuffers = function() {
+   this.vertices  = [];
+   this.indices   = [];
+   this.normals   = [];
+   this.texCoords = [];
+
+    var phi = 0.0;
+    var theta = 0.0;
+
+    for(var stack = 0; stack <= this.stacks; stack++) {
+      var y = this.radius * Math.cos(phi);
+      var v = 1 - (stack / this.stacks);
+      for(var slice = 0; slice <= this.slices; slice++) {
+          var z = this.radius * Math.sin(phi) * Math.cos(theta);
+          var x = this.radius * Math.sin(phi) * Math.sin(theta);
+          var u = 1 - (slice / this.slices);
+          this.vertices.push(x, y, z);
+          this.normals.push(x, y, z);
+          this.texCoords.push(u, v);
+          theta += 2 * Math.PI / this.slices;
+      }
+      theta = 0.0;
+      phi += Math.PI / this.stacks;
+    }
+
+    for(var stack = 0; stack < this.stacks; stack++) {
+      for(var slice = 0; slice < this.slices; slice++) {
+        var i1 = slice + stack * (this.slices + 1);
+        var i2 = slice + stack * (this.slices + 1) + 1;
+        var i3 = slice + (stack + 1) * (this.slices + 1);
+        var i4 = slice + (stack + 1) * (this.slices + 1) + 1;
+        this.indices.push(i1, i3, i4);
+        this.indices.push(i4, i2, i1);
+      }
+    }
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
 };
