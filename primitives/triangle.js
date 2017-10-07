89f171296a889e2c2dd577ce790b71110840623a
ineeve <renatoineeve@gmail.com>
2017-10-07 12:41:20 +0100

    Modified triangle to accept scaling textures and mirrored logo for spaceX

--- primitives/triangle.js
+++ primitives/triangle.js
@@ -2,11 +2,13 @@
  * Triangle
  * @constructor
  */
-function Triangle(scene, vertex_1, vertex_2, vertex_3) {
+function Triangle(scene, vertex_1, vertex_2, vertex_3, afs, aft) {
     CGFobject.call(this, scene);
-    this.v1 = vertex_1;
-    this.v2 = vertex_2;
-    this.v3 = vertex_3;
+    this.vA = vertex_1;
+    this.vB = vertex_2;
+    this.vC = vertex_3;
+    this.afs = afs || 1;
+    this.aft = aft || 1;
     this.initBuffers();
 };
 
@@ -19,32 +21,47 @@ Triangle.prototype.initBuffers = function() {
     this.normals = [];
     this.texCoords = [];
 
-    var v1 = this.v1;
-    var v2 = this.v2;
-    var v3 = this.v3;
 
-    this.vertices.push(v1[0], v1[1], v1[2]);
-    this.vertices.push(v2[0], v2[1], v2[2]);
-    this.vertices.push(v3[0], v3[1], v3[2]);
+    /**
+     * ---------------C--------------
+     * --------------/\--------------
+     * -------------/  \-------------
+     * ------------/    \------------
+     * -----------/______\-----------
+     * ----------A--------B----------
+     */
+
+    this.vertices.push(this.vA[0], this.vA[1], this.vA[2]);
+    this.vertices.push(this.vB[0], this.vB[1], this.vB[2]);
+    this.vertices.push(this.vC[0], this.vC[1], this.vC[2]);
 
     this.indices.push(0, 1, 2);
 
-    var v12 = [v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]];
-    var v13 = [v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]];
+    const AB = [this.vB[0] - this.vA[0], this.vB[1] - this.vA[1], this.vB[2] - this.vA[2]];
+    const AC = [this.vC[0] - this.vA[0], this.vC[1] - this.vA[1], this.vC[2] - this.vA[2]];
+    const BC = [this.vC[0] - this.vB[0], this.vC[1] - this.vB[1], this.vC[2] - this.vB[2]];
 
-    var normalVector = [
-        v12[1] * v13[2] - v12[2] * v13[1],
-        v12[2] * v13[0] - v12[0] * v13[2],
-        v12[0] * v13[1] - v12[1] * v13[0]
+    const ab = Math.sqrt(AB[0]*AB[0] + AB[1]*AB[1] + AB[2] * AB[2]);
+    const ac = Math.sqrt(AC[0]*AC[0] + AC[1]*AC[1] + AC[2] * AC[2]);
+    const bc = Math.sqrt(BC[0]*BC[0] + BC[1]*BC[1] + BC[2] * BC[2]);
+
+    const normalVector = [
+        AB[1] * AC[2] - AB[2] * AC[1],
+        AB[2] * AC[0] - AB[0] * AC[2],
+        AB[0] * AC[1] - AB[1] * AC[0]
     ];
 
     this.normals.push(normalVector[0], normalVector[1], normalVector[2]);
     this.normals.push(normalVector[0], normalVector[1], normalVector[2]);
     this.normals.push(normalVector[0], normalVector[1], normalVector[2]);
 
-    this.texCoords.push(0, 0);
-    this.texCoords.push(1, 0);
-    this.texCoords.push(0.5, 1);
+    //Let alpha be the angle made by the segments AB and AC
+    const cosAlpha = (-(bc*bc) + (ac*ac) + (ab*ab))/(2 * ac * ab);
+    const sinAlpha = Math.sqrt(1 - (cosAlpha*cosAlpha));
+
+    this.texCoords.push(0, 0); // Point A texture coords.
+    this.texCoords.push(ab/this.afs, 0); // Point B texture coords.
+    this.texCoords.push(ac * cosAlpha / this.afs, ac * sinAlpha / this.aft); // Point C texture coords
 
     this.primitiveType = this.scene.gl.TRIANGLES;
     this.initGLBuffers();
