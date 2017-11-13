27acf18e43c5b090c97638ce9c82e03f43bbaecb
João Carvalho <joaocarvalho9@gmail.com>
2017-11-13 16:32:49 +0000

    circularAnimationDone

--- animations/circularAnimation.js
+++ animations/circularAnimation.js
@@ -20,38 +20,32 @@ CircularAnimation.prototype.initMatrix = function() {
     let initialX = this.center[0] + Math.cos(this.initialAng) * this.radius;
     let initialZ = this.center[2] + Math.sin(this.initialAng) * this.radius;
     mat4.translate(this.initialMatrix, this.initialMatrix, [initialX, 0, initialZ]);
-    this.actualTime = 0;
-};
-
-CircularAnimation.prototype.correctionAxis = function(animationMatrix, angle) {
-    let actualAngle = this.speed / this.radius * this.actualTime;
-    if (angle < 0) {
-        actualAngle *= -1;
-    }
-    let actualX = this.center[0] + Math.cos(actualAngle) * this.radius;
-    let actualZ = this.center[2] + Math.sin(actualAngle) * this.radius;
-    mat4.translate(animationMatrix, animationMatrix, [-actualX, 0, -actualZ]);
-    mat4.rotateY(animationMatrix, animationMatrix, -angle);
-    mat4.translate(animationMatrix, animationMatrix, [actualX, 0, actualZ]);
+    this.actualAngle = this.initialAng;
 };
 
 CircularAnimation.prototype.getMatrix = function(deltaTime) {
-    let animationMatrix;
-    if (this.actualTime == 0) {
-        animationMatrix = this.initialMatrix;
-    } else {
-        animationMatrix = mat4.create();
-    }
-    this.actualTime += deltaTime;
+    let animationMatrix = mat4.create();
     let angle;
     if (this.radius != 0) {
         angle = this.speed / this.radius * deltaTime;
-        //this.correctionAxis(animationMatrix, angle); //NEED TO BE FIXED, with this rotate to the wrong side
     } else {
         angle = this.speed * deltaTime;
     }
-    mat4.translate(animationMatrix, animationMatrix, [-this.center[0], 0, -this.center[2]]);
-    mat4.rotateY(animationMatrix, animationMatrix, angle);
-    mat4.translate(animationMatrix, animationMatrix, [this.center[0], 0, this.center[2]]);
+    if (this.actualAngle + angle > this.rotationAng) {
+        angle = this.rotationAng - this.actualAngle;
+        this.actualAngle = this.rotationAng;
+    } else {
+        this.actualAngle += angle;
+    }
+    /* AXIS DONT CHANGE
+    animationMatrix = this.initialMatrix;
+    let X = Math.cos(this.actualAngle) * this.radius - Math.cos(this.actualAngle - angle) * this.radius;
+    let Z = Math.sin(this.actualAngle - angle) * this.radius - Math.sin(this.actualAngle) * this.radius;
+    mat4.translate(animationMatrix, animationMatrix, [X, 0, Z]);
+    */
+
+    /* AXIS ROTATE WITH MOVEMENT */
+    mat4.rotateY(animationMatrix, animationMatrix, this.actualAngle);
+    mat4.multiply(animationMatrix, animationMatrix, this.initialMatrix);
     return animationMatrix;
 };
