9c90b2b64176d1c532b5bdf1f1dbfc785541b8c1
João Carvalho <joaocarvalho9@gmail.com>
2017-12-12 12:42:18 +0000

    added game logic and some files to server connection

--- LatrunculiXXI.js
+++ LatrunculiXXI.js
@@ -1,67 +1,73 @@
+import { prologRequest } from "./src_Game/server_connection.js";
+
 class LatrunculiXXI {
-  constructor(game_mode) {
-    this.game_mode = game_mode;
-    this.board_stack = [];
-    this.move_stack = [];
-    this.turn = 1;
-    this.number_plays = 0;
-  }
-
-  playBot() {
-    let move = getBotMove();
-    this.board_stack[this.number_plays] = getMoveBoard(move);
-    this.move_stack[this.number_plays] = [this.turn, move];
-    this.number_plays++;
-    this.turn = (this.turn === 1) ? 2 : 1;
-  }
-
-  playHuman(xi, yi, xf, yf) {
-    if(isValidMove(xi, yi, xf, yf)) {
-      let move = [xi, yi, xf, yf];
-      this.board_stack[this.number_plays] = getMoveBoard(move);
-      this.move_stack[this.number_plays] = [this.turn, move];
-      this.number_plays++;
-      this.turn = (this.turn === 1) ? 2 : 1;
-      return true;
-    } else {
-      return false;
+    constructor(game_mode) {
+        this.game_mode = game_mode;
+        this.board_stack = [];
+        this.move_stack = [];
+        this.turn = 1;
+        this.number_plays = 0;
     }
-  }
 
-  undo() {
-    if(this.number_plays > 0) {
-      this.board_stack.pop();
-      this.move_stack.pop();
-      this.number_plays--;
-      this.turn = (this.turn === 1) ? 2 : 1;
-      return true;
-    } else {
-      return false;
+    playBot() {
+        let move = getBotMove();
+        this.board_stack[this.number_plays] = getMoveBoard(move);
+        this.move_stack[this.number_plays] = [this.turn, move];
+        this.number_plays++;
+        this.turn = (this.turn === 1) ? 2 : 1;
     }
-  }
 
-  getCurrentBoard() {
-    return this.board_stack[this.number_plays];
-  }
+    playHuman(xi, yi, xf, yf) {
+        if (isValidMove(xi, yi, xf, yf)) {
+            let move = [xi, yi, xf, yf];
+            this.board_stack[this.number_plays] = getMoveBoard(move);
+            this.move_stack[this.number_plays] = [this.turn, move];
+            this.number_plays++;
+            this.turn = (this.turn === 1) ? 2 : 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  /**
-    Inputs a move to prolog and returns the resulting board
-  */
-  getMoveBoard(move) {
-    return prologRequest({ command: 'move', args: [this.turn, move, getCurrentBoard()] });
-  }
+    undo() {
+        if (this.number_plays > 0) {
+            this.board_stack.pop();
+            this.move_stack.pop();
+            this.number_plays--;
+            this.turn = (this.turn === 1) ? 2 : 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  /**
-    Requests a valid bot move to prolog and returns it
-  */
-  getBotMove() {
-    return prologRequest({ command: 'bot_play', args: [this.turn, getCurrentBoard()] });
-  }
+    getCurrentBoard() {
+        return this.board_stack[this.number_plays];
+    }
 
-  /**
-    Inputs a move to prolog to check if it's valid
-  */
-  isValidMove(move) {
-    return prologRequest({ command: 'is_valid_move', args: [this.turn, move, getCurrentBoard()] });
-  }
+    /**
+      Inputs a move to prolog and returns the resulting board
+    */
+    getMoveBoard(move) {
+        return prologRequest({ command: 'move', args: [this.turn, move, getCurrentBoard()] });
+    }
+
+    /**
+      Requests a valid bot move to prolog and returns it
+    */
+    getBotMove() {
+        return prologRequest({ command: 'bot_play', args: [this.turn, getCurrentBoard()] });
+    }
+
+    /**
+      Inputs a move to prolog to check if it's valid
+    */
+    isValidMove(move) {
+        return prologRequest({ command: 'is_valid_move', args: [this.turn, move, getCurrentBoard()] });
+    }
+
+    testConnection() {
+        return prologRequest({ command: 'test', args: [3, 4] });
+    }
 }
