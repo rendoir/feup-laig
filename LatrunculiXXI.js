ec1a84649b6ae60484c0ffee313eb9e85026fa0f
rendoir <up201503822@fe.up.pt>
2017-12-12 11:41:13 +0000

    Added documentation

--- LatrunculiXXI.js
+++ LatrunculiXXI.js
@@ -9,8 +9,8 @@ class LatrunculiXXI {
 
   playBot() {
     let move = getBotMove();
-    this.move_stack[this.number_plays] = [this.turn, move];
     this.board_stack[this.number_plays] = getMoveBoard(move);
+    this.move_stack[this.number_plays] = [this.turn, move];
     this.number_plays++;
     this.turn = (this.turn === 1) ? 2 : 1;
   }
@@ -31,6 +31,7 @@ class LatrunculiXXI {
   undo() {
     if(this.number_plays > 0) {
       this.board_stack.pop();
+      this.move_stack.pop();
       this.number_plays--;
       this.turn = (this.turn === 1) ? 2 : 1;
       return true;
@@ -43,14 +44,23 @@ class LatrunculiXXI {
     return this.board_stack[this.number_plays];
   }
 
+  /**
+    Inputs a move to prolog and returns the resulting board
+  */
   getMoveBoard(move) {
     return prologRequest('move', this.turn, move, getCurrentBoard());
   }
 
+  /**
+    Requests a valid bot move to prolog and returns it
+  */
   getBotMove() {
     return prologRequest('bot_play', this.turn, getCurrentBoard());
   }
 
+  /**
+    Inputs a move to prolog to check if it's valid
+  */
   isValidMove(move) {
     return prologRequest('is_valid_move', this.turn, move, getCurrentBoard());
   }
