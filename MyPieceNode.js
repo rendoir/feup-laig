a556ee44355bd7097e879c808ce1e5942b9089cb
rendoir <danfil.marques@gmail.com>
2017-12-26 23:48:51 +0000

    Tried to apply an animation, not working though. Don't know why

--- MyPieceNode.js
+++ MyPieceNode.js
@@ -1,5 +1,5 @@
 class MyPieceNode extends MyGraphNode{
-    constructor(nodeID,position,type){
+    constructor(nodeID,position,type) {
         super(nodeID);
         this.type = type;
         this.position = position;
@@ -8,11 +8,22 @@ class MyPieceNode extends MyGraphNode{
         this.isPickable = true;
         this.class = "piece";
     }
-    transform(){
+
+    transform() {
         let newPosVec = vec3.fromValues(this.position.x + 0.5,0,this.position.y + 0.5);
-        mat4.fromTranslation(this.transformMatrix,newPosVec);
+        mat4.fromTranslation(this.transformMatrix, newPosVec);
+
+        //This is just a test
+        let control_points = [
+            [0, 0, 0],
+            [0, 0, 10],
+            [8, 8, 10],
+            [8, 8, 0]
+        ];
+        this.animation = new BezierAnimation(5, control_points);
     }
-    initByModel(model){
+
+    initByModel(model) {
         this.children = model.children;
         this.leaves = model.leaves;
         this.materialID = model.materialID;
